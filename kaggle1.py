ba16269ad56e7ff91b7260a86a382a882af1dd1b
Hamza-324 <hamzashujaat218@gmail.com>
2017-07-20 14:43:05 +0500

    Removing unnecessary commits

--- kaggle1.py
+++ kaggle1.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 20 11:40:15 2017
-
-@author: hamza
-"""
 
 # Classification template
 
@@ -21,23 +14,11 @@ X_train = dataset_train.iloc[:, [0, 39]].values
 y_train = dataset_train_labels.iloc[:,:].values
 X_test = dataset_test.iloc[:, [0,39]].values
 
-# Splitting the dataset into the Training set and Test set
-#from sklearn.cross_validation import train_test_split
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-
-## Feature Scaling
-#from sklearn.preprocessing import StandardScaler
-#sc = StandardScaler()
-#X_train = sc.fit_transform(X_train)
-#X_test = sc.transform(X_test)
-
 # Fitting classifier to the Training set
 # Create your classifier here
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 500,criterion = 'entropy')
 classifier.fit(X_train, y_train)
-
-
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
@@ -49,59 +30,3 @@ for i in range (0,9000):
     f.write('%d,%d' % (i+1,y_pred[i]))
     f.write('\n')
 f.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## Visualising the Training set results
-#from matplotlib.colors import ListedColormap
-#X_set, y_set = X_train, y_train
-#X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-#                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-#plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-#             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-#plt.xlim(X1.min(), X1.max())
-#plt.ylim(X2.min(), X2.max())
-#for i, j in enumerate(np.unique(y_set)):
-#    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-#                c = ListedColormap(('red', 'green'))(i), label = j)
-#plt.title('Classifier (Training set)')
-#plt.xlabel('Age')
-#plt.ylabel('Estimated Salary')
-#plt.legend()
-#plt.show()
-#
-## Visualising the Test set results
-#from matplotlib.colors import ListedColormap
-#X_set, y_set = X_test, y_test
-#X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-#                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-#plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-#             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-#plt.xlim(X1.min(), X1.max())
-#plt.ylim(X2.min(), X2.max())
-#for i, j in enumerate(np.unique(y_set)):
-#    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-#                c = ListedColormap(('red', 'green'))(i), label = j)
-#plt.title('Classifier (Test set)')
-#plt.xlabel('Age')
-#plt.ylabel('Estimated Salary')
-#plt.legend()
-#plt.show()
